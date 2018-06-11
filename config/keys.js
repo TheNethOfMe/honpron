22c440793474ca6cf8764821b74c7be336503c8a
steampunklink <nethhorn@gmail.com>
2018-06-11 09:41:37 -0700

    getting site ready for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://neth:neth@ds247569.mlab.com:47569/honpron",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
