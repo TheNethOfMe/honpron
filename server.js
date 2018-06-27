ced287deb527993a3b75d8f534fbd331f6d4a78f
steampunklink <nethhorn@gmail.com>
2018-06-27 10:32:39 -0700

    prepared for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const series = require("./routes/api/series");
@@ -30,6 +31,13 @@ app.use("/api/entries", entries);
 app.use("/api/messages", messages);
 app.use("/api/comment", comments);
 app.use("/api/tickets", tickets);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
